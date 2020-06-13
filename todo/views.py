c0c44210a715db2d66c7768f4ccccecb5cebbf97
python-the-snake <dmitriy-shalimov@yandex.ru>
2020-06-13 15:42:23 +0300

    first commit

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
-# Create your views here.
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+
+           try:
+                User.object.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html',
+                              {'form': UserCreationForm(), 'error': "That username is already been taken. Please choose a new username"})
+
+        else:
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(),
+                                                            'error':"Passwords didn't match"})
+
