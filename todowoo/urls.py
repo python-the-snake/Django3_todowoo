59eab9485e1f2515704c9d514279b2c183fb95f0
python-the-snake <dmitriy-shalimov@yandex.ru>
2020-06-17 02:00:29 +0300

    project is absolutelu here!

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -21,13 +21,16 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Auth
-    path('signup/', views.signupuser, name="signupuser"),
-    path('logout/', views.logoutuser, name="logoutuser"),
+    path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
 
     # Todos
-    path('', views.home, name="home"),
-    path('current/', views.currenttodos, name="currenttodos"),
-
-
-
+    path('', views.home, name='home'),
+    path('create/', views.createtodo, name='createtodo'),
+    path('current/', views.currenttodos, name='currenttodos'),
+    path('completed/', views.completedtodos, name='completedtodos'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
 ]
